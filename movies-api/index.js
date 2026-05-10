7c45ff2dead4f37d3501ef3d571a9680d738158d
Nathan Moran <20107318@setu.ie>
2026-05-10 18:27:14 +0100

    Added movies router; get movies from TMDB

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import './db';
 import cors from 'cors';
 import usersRouter from './api/users';
+import moviesRouter from './api/movies'; 
 import authenticate from './authenticate';
 
 dotenv.config();
@@ -28,6 +29,8 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
+
+app.use('/api/movies', moviesRouter); 
 
 app.use(errHandler);
 
